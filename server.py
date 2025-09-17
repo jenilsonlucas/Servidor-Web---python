2a2d4e6ad95a2e0123bf98cd60c849aee0a74160
Jenilson Lucas <jenilsonllucas@gmail.com>
2025-09-17 14:59:40 +0100

    feat(file static): possiblitando que o servidor sirva conteudo estatico

--- server.py
+++ server.py
@@ -1,26 +1,57 @@
 import http.server as BaseHTTPServer
+import os
+
+class ServerException(Exception):
+    pass
 
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     '''Handle HTTP requests by returning a fixed 'page'.'''
 
-    # Page to send back
-    Page = '''\
-<html>
-<body>
-<p>Hello Word, Web</p>
-</body>
-</html>
-'''
-
     # Handle get request
     def do_GET(self):
-        body = self.Page.encode('utf-8')
-        self.send_response(200)
+        try:
+            full_path = os.getcwd() + self.path
+            
+            if not os.path.exists(full_path):
+                raise ServerException("'{0}' not found".format(self.path))
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+      
+    Error_page = """\
+        <html>
+        <body>
+        <h1>Error acessing {path}</h1>
+        <p>{msg}</p>
+        </body>
+        </html>
+    """
+
+    def handle_error(self, msg):
+        content = self.Error_page.format(path=self.path, msg=msg)
+        self.send_content(content, 404)
+
+    def send_content(self, content, status=200):
+        body = content.encode('utf-8')
+        self.send_response(status)
         self.send_header("content-type", "text/html")
         self.send_header("content-length", str(len(body)))
         self.end_headers()
         self.wfile.write(body)
+    
 
 
 if __name__ == '__main__':
