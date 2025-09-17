45a265302dbf2f4646dede21e9bec03010da37ae
Jenilson Lucas <jenilsonllucas@gmail.com>
2025-09-17 16:52:08 +0100

    feat(list directories): listando arquivos de uma pasta
    permitir que o servidor seje capaz de listar os arquivos de uma pasta
    se o usuario fizer a requisição de uma pasta e não de um aquivo

--- server.py
+++ server.py
@@ -8,19 +8,19 @@ class case_no_file(object):
     def test(self, handler):
         return not os.path.exists(handler.full_path)
     def act(self, handler):
-        raise ServerException("'{0}' not found".format(self.path))
+        raise ServerException("'{0}' not found".format(handler.path))
 
 class case_existing_file(object):
     def test(self, handler):
         return os.path.isfile(handler.full_path)
     def act(self, handler):
-        handler.handle_file(handler.full_fath)
+        handler.handle_file(handler.full_path)
 
 class case_always_fail(object):
-    def test(self,handler):
+    def test(self, handler):
         return True
     def act(self, handler):
-        raise ServerException("Unknown object '{0}'".format(self.path))
+        raise ServerException("Unknown object '{0}'".format(handler.path))
 
 class case_directory_index_file(object):
 
@@ -32,12 +32,21 @@ class case_directory_index_file(object):
     def act(self, handler):
         handler.handle_file(self.index_path(handler))
 
+class case_directory_no_index_file(object):
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+    def test(self, handler):
+        return os.path.isdir(handler.full_path) and \
+           not os.path.isfile(self.index_path(handler))
+    def act(self, handler):
+        handler.list_dir(handler.full_path)
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
    
     Cases = [case_no_file(), 
         case_existing_file(),
         case_directory_index_file(),
+        case_directory_no_index_file(),
         case_always_fail()]
     
     full_path = ""
@@ -76,12 +85,33 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_content(content, 404)
 
     def send_content(self, content, status=200):
+        if(isinstance(content, str)):
+            content = content.encode('utf-8')
         self.send_response(status)
         self.send_header("content-type", "text/html")
         self.send_header("content-length", str(len(content)))
         self.end_headers()
         self.wfile.write(content)
-    
+
+    Listing_Page = '''\
+        <html>
+        <body>
+        <ul>
+        {0}
+        </ul>
+        </body>
+        </html>
+        '''
+
+    def list_dir(self, full_path):
+        try:
+            entries = os.listdir(full_path)
+            bullets = ['<li>{0}</li>'.format(e) for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            self.send_content(page)
+        except OSError as msg:
+            msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
+            self.handle_error(msg)
 if __name__ == '__main__':
     serverAddress = ('', 8080)
     server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
